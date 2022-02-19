dbcd088fb857b5b8e4ff74d2d86f5a6814fe00a0
Avo_Mandjian <avo@thewebaddicts.com>
2022-02-19 16:08:55 +0200

    ref

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from model_bakery.recipe import Recipe, foreign_key
+# from faker import Faker
+# from store.models import *
+# # this Populator is only a function thats return a django_faker.populator.Populator instance
+# # correctly initialized with a faker.generator.Generator instance, configured as above
+# fake = Faker()
 
-# Register your models here.
+# author = Recipe(Promotion, description=fake.name(),
+#                 discount=fake.random_int())
+# author.make()
